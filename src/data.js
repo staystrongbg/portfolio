fb896003f6065693751aabff2ca82c0075db60e3
ddamaja <79502735+staystrongbg@users.noreply.github.com>
2022-01-12 11:21:28 +0000

    Add files via upload

--- src/data.js
+++ src/data.js
@@ -1,15 +1,5 @@
 export const products = [
   {
-    id: 1,
-    img: './images/Screenshot from 2021-10-28 16-15-39.png',
-    link: 'http://sala-ketering-kameleon.com',
-  },
-  {
-    id: 2,
-    img: './images/ciklogen.png',
-    link: 'https://awesome-mountain.surge.sh',
-  },
-  {
     id: 3,
     img: './images/sikimoto.png',
     link: 'https://sikimoto-app.netlify.app',
